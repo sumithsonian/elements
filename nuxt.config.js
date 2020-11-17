9fd897da3a7bfb472a91da9a4f422ae89bdc9ed1
角雄崇 <sumi@ini.co.jp>
2020-11-17 14:13:40 +0900

    [add]元となるのscss追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/scss/style.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
