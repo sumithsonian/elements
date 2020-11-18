8cdb33d99aec59dd357f8682a8982b45ea87d5cc
角雄崇 <sumi@ini.co.jp>
2020-11-18 14:54:36 +0900

    [add]画像コンポーネントの追加

---
+++ components/elements/Img.vue
@@ -0,0 +1,21 @@
+<template>
+  <img :src="src" :alt="alt" class="img" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true },
+    alt: { type: String, required: false, default: 'img' },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/scss/_settings.scss';
+
+.img {
+  display: inline-block;
+  border-radius: $radius;
+}
+</style>
