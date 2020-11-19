4e3f19cb83356150d1dcfba5918b4bf6f5fa8a3c
角雄崇 <sumi@ini.co.jp>
2020-11-19 13:46:37 +0900

    [add]見出し追加

---
+++ components/elements/Heading.vue
@@ -0,0 +1,40 @@
+<template>
+  <compnent :is="tagName" :class="headingClass"><slot /></compnent>
+</template>
+
+<script>
+export default {
+  props: {
+    level: { type: Number, required: true },
+  },
+  computed: {
+    headingClass() {
+      return 'heading--' + this.level
+    },
+    tagName() {
+      return 'h' + this.level
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/scss/_settings.scss';
+
+.heading {
+  &--1 {
+    font-size: $fontXXL;
+  }
+  &--2 {
+    font-size: $fontXL;
+  }
+  &--3 {
+    font-size: $fontL;
+  }
+  &--4,
+  &--5,
+  &--6 {
+    font-size: $font;
+  }
+}
+</style>
