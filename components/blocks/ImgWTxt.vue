aec72a9cec5584f33bc16b09443a5d13cef86572
角雄崇 <sumi@ini.co.jp>
2020-11-20 15:14:45 +0900

    [add]画像+テキストコンポーネント追加

---
+++ components/blocks/ImgWTxt.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="imgWTxt">
+    <div class="imgWTxt__img">
+      <slot name="img" />
+    </div>
+    <div class="imgWTxt__txt">
+      <slot name="txt" class="imgWTxt__txt" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/scss/_settings.scss';
+
+.imgWTxt {
+  display: flex;
+  &__txt {
+    margin-left: $space;
+  }
+}
+</style>
